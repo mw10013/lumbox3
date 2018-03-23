069017ae96a0c0246132873dd1f0d4390b97784e
Michael Wu <mw10013@gmail.com>
2018-03-23 11:43:27 -0400

    added graphql request handler

--- src/clj/lumbox3/routes/services.clj
+++ src/clj/lumbox3/routes/services.clj
@@ -2,11 +2,10 @@
   (:require [com.walmartlabs.lacinia.util :as lacinia-util]
             [com.walmartlabs.lacinia.schema :as schema]
             [com.walmartlabs.lacinia :as l]
-            [clojure.data.json :as json]
             [clojure.edn :as edn]
-            [ring.util.http-response :refer :all]
             [compojure.api.sweet :refer :all]
-            [mount.core :as mount]))
+            [mount.core :as mount]
+            [ring.util.http-response :as response]))
 
 (defn get-hero [context args value]
   (let [data  [{:id 1000
@@ -28,21 +27,36 @@
                         :get-droid (constantly {})})
                      schema/compile))
 
-(defn format-params [query]
-   (let [parsed (json/read-str query)] ;;-> placeholder - need to ensure query meets graphql syntax
-     (str "query { hero(id: \"1000\") { name appears_in }}")))
-
-(defn execute-request [query]
-    (let [vars nil
-          context nil]
-    (-> (l/execute compiled-schema query vars context)
-        (json/write-str))))
+(defn graphql
+  "Handles graphql request.
+   Adds :session and :side-effects kyes to the lacinia context map.
+   Session comes from the request and side-effects is an atom map.
+   To change the session on the way out, add a session key to side effects.
+   A nil value will clear the session."
+  [req]
+  (let [{{:keys [query variables operation-name operationName]} :params} req
+        query (or query (-> req :body slurp))
+        _ (println "graphql: query:" query)
+        options {:operation-name (or operation-name operationName)}
+        side-effects (atom {})
+        context {:session (:session req) :side-effects side-effects}
+        result (l/execute schema query variables context options)
+        _ (println "graphql: result:" result)
+        response (if (:errors result) (response/bad-request result) (response/ok result))
+        session (:session @side-effects ::not-found)]       ; nil will clear the session.
+    (if (= session ::not-found) response (assoc response :session session))))
 
 (defapi service-routes
-  (POST "/api" [:as {body :body}]
-      (ok (execute-request (slurp body)))))
+  (POST "/api" req (graphql req))
+  (GET "/api" req (graphql req)))
 
 (comment
   (mount.core/start #'lumbox3.routes.services/graphql-schema)
   (l/execute schema "{ hero(id: \"id\") { id name appears_in } }" nil nil)
+
+  (require 'ring.mock.request)
+  (ring.mock.request/request :post "/api" {:query "{ hero(id: \"id\") { id name appears_in } }"})
+  (service-routes (ring.mock.request/request :post "/api" {:query "{ hero(id: \"id\") { id name appears_in } }"}))
+  (-> (ring.mock.request/request :post "/api" {:query "{ hero(id: \"id\") { id name appears_in } }"})
+      service-routes :body slurp)
   )
