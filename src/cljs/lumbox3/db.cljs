a5477e5f80b27f9fd08484d239a8e42971048977
Michael Wu <mw10013@gmail.com>
2018-04-01 22:27:00 -0400

    header identity logic

--- src/cljs/lumbox3/db.cljs
+++ src/cljs/lumbox3/db.cljs
@@ -1,4 +1,6 @@
 (ns lumbox3.db)
 
+; re-frame.db/app-db
+
 (def default-db
   {:main-view :home})
--- src/cljs/lumbox3/views.cljs
+++ src/cljs/lumbox3/views.cljs
@@ -10,61 +10,6 @@
 (def antd js/antd)
 (def m js/moment)
 
-(defn header []
-  [:div "header"]
-  #_[:> sui.Menu {:inverted true}
-   [:> sui.Container
-    #_[:> sui.Menu.Item {:as "a" :header true :link true :href "/#"} "Lumbox 3"]
-    [:> sui.Menu.Item {:as "a" :header true :on-click #(rf/dispatch [::events/set-main-view :home])} "Lumbox 3"]
-    [:> sui.Menu.Item {:as "a" :on-click #(rf/dispatch [::events/set-main-view :home])} "Home"]
-    [:> sui.Dropdown {:item true :simple true :text "Dropdown"}
-     [:> sui.Dropdown.Menu
-      #_[:> sui.Dropdown.Item {:value   (pr-str [::events/set-main-view :about])
-                               :onClick #(-> %2 (aget "value") reader/read-string rf/dispatch)} "About"]
-      [:> sui.Dropdown.Item {:on-click #(rf/dispatch [::events/set-main-view :about])} "About"]
-      [:> sui.Dropdown.Item {:onClick #(rf/dispatch [::events/set-main-view :home])} "Home"]
-      [:> sui.Dropdown.Divider]
-      [:> sui.Dropdown.Item
-       [:i.dropdown.icon]
-       [:span.text "Submenu"]
-       [:> sui.Dropdown.Menu
-        [:> sui.Dropdown.Item {:value   (pr-str [::events/set-active-panel :simple-sui-panel])
-                               :onClick #(-> %2 (aget "value") reader/read-string rf/dispatch)} "Simple"]
-        [:> sui.Dropdown.Item {:value   (pr-str [::events/set-active-panel :simple-sui-react-panel])
-                               :onClick #(-> %2 (aget "value") reader/read-string rf/dispatch)} "Simple React"]]]]]
-    [:> sui.MenuItem {:position :right}
-     [:> sui.Button {:as :a :inverted true :on-click #(rf/dispatch [::events/set-main-view :login])} "Log in"]
-     [:> sui.Button {:as       :a :inverted true :style {:marginLeft "0.5em"}
-                     :on-click #(rf/dispatch [::events/set-main-view :register])} "Register"]]]])
-
-(defn footer []
-  #_[:> sui.Segment {:inverted true :vertical true :style {:padding "5em 0em"}}
-   [:> sui.Container
-    [:> sui.Grid {:divided true :inverted true :stackable true}
-     [:> sui.Grid.Row
-      [:> sui.Grid.Column {:width 3}
-       [:> sui.Header {:as :h4 :inverted true :content "About"}]
-       [:> sui.List {:link true :inverted true}
-        [:> sui.List.Item {:as :a} "Sitemap"]
-        [:> sui.List.Item {:as :a} "Contact"]]]
-      [:> sui.Grid.Column {:width 3}
-       [:> sui.Header {:as :h4 :inverted true} "Services"]
-       [:> sui.List {:link true :inverted true}
-        [:> sui.List.Item {:as :a} "Pre-Order"]
-        [:> sui.List.Item {:as :a} "FAQ"]]]
-      [:> sui.Grid.Column {:width 7}
-       [:> sui.Header {:as :h4 :inverted true} "Footer Header"]
-       [:p "Extra space for a call to action inside the footer that could help re-engage users."]]]]]]
-  [:> antd.Layout.Footer {:style {:text-align :center}} "Footer"])
-
-(defn home-view []
-  [:div
-   [:h1 "Home"]])
-
-(defn about-view []
-  [:div
-   [:h1 "About"]])
-
 (defn debug-cache [cache-key]
   [:div [:hr]
    [:div "cache-key: " cache-key ": " (pr-str @(rf/subscribe [:cache cache-key]))]
@@ -75,44 +20,52 @@
 (defn register-view []
   [:div "register-view"]
   #_(let [cache-key :register
-        input @(rf/subscribe [:input cache-key])
-        input-errors @(rf/subscribe [:input-errors cache-key])
-        error-message @(rf/subscribe [:error-message cache-key])]
-    [:> sui.Grid {:textAlign "center" #_:style #_{:height "100%"} :verticalAlign "middle"}
-     [:> sui.Grid.Column {:style {:maxWidth 450}}
-      [:> sui.Header {:as :h2 :color "teal" :textAlign "center"} "Register for an account"]
-      [:> sui.Form {:size :large
-                    :on-submit (fn [e]
-                            (.preventDefault e)
-                            (.stopPropagation e)
-                            (let [[input-errors input] (v/validate-register-user-input @(rf/subscribe [:input cache-key]))]
-                              (rf/dispatch [:set-input-errors cache-key input-errors])
-                              #_(when-not input-errors
-                                (rf/dispatch [:register-user cache-key input]))))}
-       [:> sui.Segment {:stacked true}
-        [:> sui.Form.Input {:fluid     true :icon :user :iconPosition :left :placeholder "E-mail address"
-                            :value     (:email input) :label "E-mail" :required true
-                            :on-change #(rf/dispatch [:set-input cache-key :email (-> % .-target .-value)])}]
-        [:> sui.Form.Input {:fluid     true :icon :lock :iconPosition :left :placeholder "Password" :type :password
-                            :value     (:password input)
-                            :on-change #(rf/dispatch [:set-input cache-key :password (-> % .-target .-value)])}]
-        [:> sui.Button {:color :teal :fluid true :size :large} "Register"]
-        [debug-cache cache-key]]]]]))
+          input @(rf/subscribe [:input cache-key])
+          input-errors @(rf/subscribe [:input-errors cache-key])
+          error-message @(rf/subscribe [:error-message cache-key])]
+      [:> sui.Grid {:textAlign "center" #_:style #_{:height "100%"} :verticalAlign "middle"}
+       [:> sui.Grid.Column {:style {:maxWidth 450}}
+        [:> sui.Header {:as :h2 :color "teal" :textAlign "center"} "Register for an account"]
+        [:> sui.Form {:size      :large
+                      :on-submit (fn [e]
+                                   (.preventDefault e)
+                                   (.stopPropagation e)
+                                   (let [[input-errors input] (v/validate-register-user-input @(rf/subscribe [:input cache-key]))]
+                                     (rf/dispatch [:set-input-errors cache-key input-errors])
+                                     #_(when-not input-errors
+                                         (rf/dispatch [:register-user cache-key input]))))}
+         [:> sui.Segment {:stacked true}
+          [:> sui.Form.Input {:fluid     true :icon :user :iconPosition :left :placeholder "E-mail address"
+                              :value     (:email input) :label "E-mail" :required true
+                              :on-change #(rf/dispatch [:set-input cache-key :email (-> % .-target .-value)])}]
+          [:> sui.Form.Input {:fluid     true :icon :lock :iconPosition :left :placeholder "Password" :type :password
+                              :value     (:password input)
+                              :on-change #(rf/dispatch [:set-input cache-key :password (-> % .-target .-value)])}]
+          [:> sui.Button {:color :teal :fluid true :size :large} "Register"]
+          [debug-cache cache-key]]]]]))
 
 (defn login-view []
   [:div "login-view"]
   #_[:> sui.Grid {:textAlign "center" :style {:height "100%"} :verticalAlign "middle"}
-   [:> sui.Grid.Column {:style {:maxWidth 450}}
-    [:> sui.Header {:as :h2 :color "teal" :textAlign "center"} "Log in to your account"]
-    [:> sui.Form {:size :large}
-     [:> sui.Segment {:stacked true}
-      [:> sui.Form.Input {:fluid true :icon :user :iconPosition :left :placeholder "E-mail address"}]
-      [:> sui.Form.Input {:fluid true :icon :lock :iconPosition :left :placeholder "Password" :type :password}]
-      [:> sui.Button {:color :teal :fluid true :size :large} "Login"]]]]])
+     [:> sui.Grid.Column {:style {:maxWidth 450}}
+      [:> sui.Header {:as :h2 :color "teal" :textAlign "center"} "Log in to your account"]
+      [:> sui.Form {:size :large}
+       [:> sui.Segment {:stacked true}
+        [:> sui.Form.Input {:fluid true :icon :user :iconPosition :left :placeholder "E-mail address"}]
+        [:> sui.Form.Input {:fluid true :icon :lock :iconPosition :left :placeholder "Password" :type :password}]
+        [:> sui.Button {:color :teal :fluid true :size :large} "Login"]]]]])
 
 (defn logout-view []
   [:div
    [:h1 "Log out"]])
+
+(defn home-view []
+  [:div
+   [:h1 "Home"]])
+
+(defn about-view []
+  [:div
+   [:h1 "About"]])
 
 (def main-views
   {:home     home-view
@@ -125,23 +78,34 @@
   (let [k @(rf/subscribe [::events/main-view])]
     [(get main-views k #(vector :div (str "main-view not found: " k)))]))
 
+(defn header []
+  (let [identity @(rf/subscribe [:identity])]
+    [:> antd.Layout.Header
+     [:> antd.Menu {:theme :dark :mode :horizontal :selectable false
+                    :style {:line-height "64px"}}
+      [:> antd.Menu.Item
+       [:a {:href "#/"}] "Lumbox 3"]
+      [:> antd.Menu.Item
+       [:a {:href "#/about"}] "About"]
+      [:> antd.Menu.SubMenu {:title "Admin"}
+       [:> antd.Menu.Item "Users"]
+       [:> antd.Menu.Item "Groups"]]
+      ; float right appears in opposite order ie. Login Register
+      (when-not identity [:> antd.Menu.Item {:style {:float :right}}
+                          [:a {:href "#/register"} "Register"]])
+      (if identity
+        [:> antd.Menu.Item {:style {:float :right}}
+         [:a {:href "#/logout"} "Logout"]]
+        [:> antd.Menu.Item {:style {:float :right}}
+         [:a {:href "#/login"} "Login"]])]]))
+
+(defn footer []
+  [:> antd.Layout.Footer {:style {:text-align :center}} "Footer"])
+
 (defn root-view []
-  [:> antd.Layout
-   [:> antd.Layout.Header
-    [:> antd.Menu {:theme :dark :mode :horizontal :selectable false
-                   :style {:line-height "64px"}}
-     [:> antd.Menu.Item
-      [:a {:href "#/"}] "Lumbox 3"]
-     [:> antd.Menu.Item
-      [:a {:href "#/about"}] "About"]
-     [:> antd.Menu.SubMenu {:title "Admin"}
-      [:> antd.Menu.Item "Users"]
-      [:> antd.Menu.Item "Groups"]]
-     ; float right appears in opposite order ie. Login Register
-     [:> antd.Menu.Item {:style {:float :right}}
-      [:a {:href "#/register"} "Register"]]
-     [:> antd.Menu.Item {:style {:float :right}}
-      [:a {:href "#/login"} "Login"]]]]
-   [:> antd.Layout.Content {:style {:margin 0 :padding "2em" :border "0px solid black"}}
-    [main-view]]
-   [footer]])
+  (let [identity @(rf/subscribe [:identity])]
+    [:> antd.Layout
+     [header]
+     [:> antd.Layout.Content {:style {:margin 0 :padding "2em" :border "0px solid black"}}
+      [main-view]]
+     [footer]]))
